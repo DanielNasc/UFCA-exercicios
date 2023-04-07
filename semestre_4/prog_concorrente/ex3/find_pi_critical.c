b80e4bc49b7c62e5d0fd2d7e44962a109d061bad
DanielNasc <danielnasc15987@gmail.com>
2023-04-07 14:37:41 -0300

    find pi critical/atomic

---
+++ semestre_4/prog_concorrente/ex3/find_pi_critical.c
@@ -0,0 +1,108 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define print_result(result, time) \
+    printf("pi = %6f, time = %6f\n", result, time);
+
+double pi_serial(int N);
+double pi_critical(int N);
+double pi_atomic(int N);
+
+int main() {
+    double pi, start, end;
+    int N = 100000000;
+
+    printf("Parallelizing with critical\n");
+
+    start = omp_get_wtime();
+    pi = pi_critical(N);
+    end = omp_get_wtime();
+
+    print_result(pi, end - start);
+
+    printf("Parallelizing with atomic\n");
+
+    start = omp_get_wtime();
+    pi = pi_atomic(N);
+    end = omp_get_wtime();
+
+    print_result(pi, end - start);
+}
+
+double f(double x) 
+{
+    return 4.0 / (1.0 + x * x);
+}
+
+double pi_serial(int N) 
+{
+    double step = 1.0 / (double) N;
+    double pi;
+    int i;
+
+    pi = 0.0;
+    for (i = 0; i < N; i++) {
+        double x = (i + 0.5) * step;
+        pi += f(x);
+    }
+    pi *= step;
+
+    return pi;
+}
+
+double pi_critical(int N) 
+{
+    double step = 1.0 / (double) N;
+    double pi;
+    int procs = omp_get_num_procs();
+
+    pi = 0.0;
+    
+    #pragma omp parallel num_threads(procs)
+    {
+        double x, sum = 0.0;
+        int i, id, nthrds;
+
+        id = omp_get_thread_num();
+        nthrds = omp_get_num_threads();
+
+        for (i = id; i < N; i += nthrds) {
+            x = (i + 0.5) * step;
+            sum += f(x);
+        }
+
+        #pragma omp critical
+            pi += sum * step;
+    }
+
+    return pi;
+}
+
+double pi_atomic(int N) 
+{
+    double step = 1.0 / (double) N;
+    double pi;
+    int procs = omp_get_num_procs();
+
+    pi = 0.0;
+    
+    #pragma omp parallel num_threads(procs)
+    {
+        double x, sum = 0.0;
+        int i, id, nthrds;
+
+        id = omp_get_thread_num();
+        nthrds = omp_get_num_threads();
+
+        for (i = id; i < N; i += nthrds) {
+            x = (i + 0.5) * step;
+            sum += f(x);
+        }
+
+        #pragma omp atomic
+            pi += sum * step;
+    }
+
+    return pi;
+}
