c711afe1be0680f7dc542db89583fceae874b60e
DanielNasc <danielnasc15987@gmil.com>
2023-05-25 13:57:21 -0300

    Fixed: Divide variance by total size instead of by partial size

--- semestre_4/prog_concorrente/ex12/ex12.c
+++ semestre_4/prog_concorrente/ex12/ex12.c
@@ -4,34 +4,43 @@
 #include <math.h>
 #include <time.h>
 
-double mean(int *X, int N) {
+#define SIZE 100
+#define RANDOM_VAR_MAX 50
+
+double mean(int *X) {
     int m = 0;
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < SIZE; i++) {
         m += X[i];
     }
 
-    return (double) m / (double) N;
+    return (double) m / (double) SIZE;
 }
 
 double variance(int *X, int N, double mean) {
     double sd = 0;
 
     for (int i = 0; i < N; i++) {
-        sd += pow(((double) X[i] - mean), 2)/N;
+        sd += pow(((double) X[i] - mean), 2)/SIZE;
     }
 
     return sd;
 }
 
-int *createRandomVariable(int N, int max) {
-    int *X = (int *) malloc(sizeof(int) * N);
+int *createRandomVariable() {
+    int *X = (int *) malloc(sizeof(int) * SIZE);
 
-    for (int i = 0; i < N; i++) {
-        X[i] = (rand() % max);
+    for (int i = 0; i < SIZE; i++) {
+        X[i] = (rand() % RANDOM_VAR_MAX);
     }
 
     return X;
+}
+
+double standartDeviationSerial(int *X, double m) {
+    double sd = variance(X, SIZE, m);
+
+    return sqrt(sd);
 }
 
 int main() {
@@ -41,15 +50,31 @@ int main() {
     int my_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    int N = 100, N_sub = 5;
+    int N_sub = SIZE / n_cpus;
     int *X, *subArrX;
-    double m, v, *results = NULL;
+    double  m, v, *results = NULL, 
+            start_serial, end_serial,
+            start_mpi, end_mpi;
 
     if (my_rank == 0) {
         srand(time(NULL));
         
-        X = createRandomVariable(N, 50);
-        m = mean(X, N);
+        X = createRandomVariable();
+        m = mean(X);
+
+        printf("Média: %f\n", m);
+
+        printf("\n");
+        printf("Começando o cálculo do desvio padrão de forma serial\n");
+        start_serial = MPI_Wtime();
+        double serialSd = standartDeviationSerial(X, m);
+        end_serial = MPI_Wtime();
+        printf("Desvio Padrão: %f\n", serialSd);
+        printf("Tempo decorrido: %f\n", end_serial - start_serial);
+
+        printf("\n");
+        printf("Começando o cálculo do desvio padrão de forma paralela\n");
+        start_mpi = MPI_Wtime();
     }
 
     subArrX = (int *) malloc(sizeof(int) * N_sub);
@@ -69,12 +94,15 @@ int main() {
         double sd = 0;
 
         for (int i = 0; i < n_cpus; i++) {
-            sd += results[0];
+            sd += results[i];
         }
 
         sd = sqrt(sd);
 
+        end_mpi = MPI_Wtime();
+
         printf("Desvio Padrão: %f\n", sd);
+        printf("Tempo decorrido: %f\n", end_mpi - start_mpi);
     }
 
     MPI_Finalize();
